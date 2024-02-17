8cda76d95447672253597ee864e19a4144f27ccf
luckylinux <luckylinux>
2024-02-17 21:22:52 +0100

    Add rules to scripts meant to be EXCLUSIVELY executed within the chroot environment

--- inside-chroot/setup_ubuntu_desktop.sh
+++ inside-chroot/setup_ubuntu_desktop.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Make sure we are in chroot
+# Abort if we are trying to run the script from the host machine
+if [ "$(stat -c %d:%i /)" == "$(stat -c %d:%i /proc/1/root/.)" ] 
+then
+        echo "This script must ONLY be run within the chroot environment. Aborting !"
+        exit 2
+fi
+
 # If toolpath not set, set it to current working directory
 if [[ ! -v toolpath ]]
 then
