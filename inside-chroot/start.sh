3996ac701983eae4a1f168da963ff97df1b90fb4
luckylinux <luckylinux>
2024-02-18 19:34:45 +0100

    Playing a bit around with relative paths in order to determine $toolpath, whether one script is invoked from the root folder or from a subfolder, the result should be the same. It's not working yet :/

--- inside-chroot/start.sh
+++ inside-chroot/start.sh
@@ -1,17 +1,42 @@
 #!/bin/bash
 
-# Make sure we are in chroot
-# Abort if we are trying to run the script from the host machine
-if [ "$(stat -c %d:%i /)" == "$(stat -c %d:%i /proc/1/root/.)" ] 
-then
-        echo "This script must ONLY be run within the chroot environment. Aborting !"
-        exit 2
-fi
+# Used if we can run directly a script when entering chroot
+# So that the user doesn't need to manually source /etc/profile within the chroon and
+# cd to /tools_install/$timestamp
 
 # Determine toolpath
 scriptpath=$(dirname "${BASH_SOURCE[0]}")
 parentpath=$(dirname "$scriptpath")
-toolpath="$( cd "$( dirname "$scriptpath" )" > /dev/null && pwd )"
+toolpath1="$( cd "$( dirname "$scriptpath" )" > /dev/null && pwd )"
+
+# Relativefolder
+relativefolder=".."
+
+# Need to strip first $0 of ./
+arg=$0
+arg=${arg//.\//""}
+
+# Determine subfolder based on script invocation call
+subfolder=$(dirname $arg)
+
+relativepath=$(dirname $relativefolder/$arg)
+relativepath=$(realpath --canonicalize-missing ${relativepath})
+
+# Remove subfolder name if present in name
+toolpath2=$(realpath --canonicalize-missing ${relativepath//${subfolder}/""})
+
+# Return shortest string
+if [ ${#toolpath1} -lt ${#toolpath2} ]; then toolpath=$toolpath1; else toolpath=$toolpath2; fi
+echo "Tool path: $toolpath"
+
+
+# Make sure we are in chroot
+# Abort if we are trying to run the script from the host machine
+if [ "$(stat -c %d:%i /)" == "$(stat -c %d:%i /proc/1/root/.)" ]
+then
+        echo "This script must ONLY be run within the chroot environment. Aborting !"
+        exit 2
+fi
 
 echo "Starting script"
 
