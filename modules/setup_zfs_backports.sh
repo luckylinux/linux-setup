afdccf7a3f18f4492b4817b0acb042786b1a4fe7
luckylinux <luckylinux>
2024-04-22 17:00:22 +0200

    Fixing Path Separator Missing in Script.

--- modules/setup_zfs_backports.sh
+++ modules/setup_zfs_backports.sh
@@ -17,13 +17,13 @@ mkdir -p ${installroot}/etc/apt/preferences.d
 cp ${toolpath}/repositories/${distribution}/${release}/sources.list.d/${distribution}-backports.list ${installroot}/etc/apt/sources.list.d/${distribution}-backports.list
 
 # By default do NOT use backports
-cp ${toolpath}/repositories/${distribution}${release}/preferences.d/${distribution}-backports ${installroot}/etc/apt/preferences.d/${distribution}-backports
+cp ${toolpath}/repositories/${distribution}/${release}/preferences.d/${distribution}-backports ${installroot}/etc/apt/preferences.d/${distribution}-backports
 
 # Copy Kernel Backports Configuration
-cp ${toolpath}/repositories/${distribution}${release}/preferences.d/kernel-backports ${installroot}/etc/apt/preferences.d/kernel-backports
+cp ${toolpath}/repositories/${distribution}/${release}/preferences.d/kernel-backports ${installroot}/etc/apt/preferences.d/kernel-backports
 
 # Copy ZFS Backports Configuration
-cp ${toolpath}/repositories/${distribution}${release}/preferences.d/zfs-backports ${installroot}/etc/apt/preferences.d/zfs-backports
+cp ${toolpath}/repositories/${distribution}/${release}/preferences.d/zfs-backports ${installroot}/etc/apt/preferences.d/zfs-backports
 
 # Update Sources
 apt-get update
