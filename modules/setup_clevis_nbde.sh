3de433eb4f21fc2302be7d9b2c0eb86a3f6c565b
luckylinux <luckylinux>
2025-02-09 17:31:52 +0100

    Remove OLD style Loop for Clevis Setup.

--- modules/setup_clevis_nbde.sh
+++ modules/setup_clevis_nbde.sh
@@ -68,41 +68,54 @@ apt-get install --yes clevis clevis-luks clevis-initramfs cryptsetup-initramfs
 # Ask for password
 read -s -p "Enter encryption password: " password
 
-# For each keyserver
-counter=1
-for keyserver in "${keyservers[@]}"
-do
-     # Get TANG Server Key
-     curl -sfg http://$keyserver/adv -o /tmp/keyserver-$counter.jws
+# (OLD LOOP) For each keyserver
+# counter=1
+# for keyserver in "${keyservers[@]}"
+# do
+#     # Get TANG Server Key
+#     curl -sfg http://$keyserver/adv -o /tmp/keyserver-$counter.jws
+# 
+#      # Check which keys are currently used via CLEVIS
+#      list_device1=$(clevis luks list -d $device1-part${root_num})
+#      list_device2=$(clevis luks list -d $device2-part${root_num})
+# 
+#      # Bind device to the TANG server via CLEVIS
+#      # Device 1
+#      if [[ "${list_device1}" == *"${keyserver}"* ]]
+#      then
+#          echo "Keyserver <$keyserver> is already installed"
+#      else
+#          echo "Install Keyserver <$keyserver> onto $device1 LUKS Header"
+#          # echo $password | clevis luks bind -d $device1-part${root_num} tang "{\"url\": \"http://$keyserver\" , \"adv\": \"/tmp/keyserver-$counter.jws\" }"
+#          echo $password | clevis luks bind -d $device1-part${root_num} sss ${tangkeyserverdict}
+#      fi
+# 
+#     # Device 2
+#      if [[ "${list_device2}" == *"${keyserver}"* ]]
+#      then
+#          echo "Keyserver <$keyserver> is already installed"
+#      else
+#          echo "Install Keyserver <$keyserver> onto $device2 LUKS Header"
+#          # echo $password | clevis luks bind -d $device2-part${root_num} tang "{\"url\": \"http://$keyserver\" , \"adv\": \"/tmp/keyserver-$counter.jws\" }"
+#          echo $password | clevis luks bind -d $device2-part${root_num} sss ${tangkeyserverdict}
+#      fi
+# 
+#      # Increment counter
+#      counter=$((counter+1))
+# done
 
-     # Check which keys are currently used via CLEVIS
-     list_device1=$(clevis luks list -d $device1-part${root_num})
-     list_device2=$(clevis luks list -d $device2-part${root_num})
 
-     # Bind device to the TANG server via CLEVIS
-     # Device 1
-     if [[ "${list_device1}" == *"${keyserver}"* ]]
-     then
-         echo "Keyserver <$keyserver> is already installed"
-     else
-         echo "Install Keyserver <$keyserver> onto $device1 LUKS Header"
-         # echo $password | clevis luks bind -d $device1-part${root_num} tang "{\"url\": \"http://$keyserver\" , \"adv\": \"/tmp/keyserver-$counter.jws\" }"
-         echo $password | clevis luks bind -d $device1-part${root_num} sss ${tangkeyserverdict}
-     fi
 
-     # Device 2
-     if [[ "${list_device2}" == *"${keyserver}"* ]]
-     then
-         echo "Keyserver <$keyserver> is already installed"
-     else
-         echo "Install Keyserver <$keyserver> onto $device2 LUKS Header"
-         # echo $password | clevis luks bind -d $device2-part${root_num} tang "{\"url\": \"http://$keyserver\" , \"adv\": \"/tmp/keyserver-$counter.jws\" }"
-         echo $password | clevis luks bind -d $device2-part${root_num} sss ${tangkeyserverdict}
-     fi
+# Use pre-build Dictionary
+echo "Install Keyservers onto $device1 LUKS Header"
+echo ${tangkeyserverdict} | jq -r --color-output
+echo $password | clevis luks bind -d $device1-part${root_num} sss ${tangkeyserverdict}
 
-     # Increment counter
-     counter=$((counter+1))
-done
+
+echo "Install Keyservers onto $device2 LUKS Header"
+echo ${tangkeyserverdict} | jq -r --color-output
+echo $password | clevis luks bind -d $device2-part${root_num} sss ${tangkeyserverdict}
+
 
 # Clear password from memory
 unset $password
