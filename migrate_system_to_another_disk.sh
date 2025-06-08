34a77448210c541e7923a9a45ae40e614f3c9aa2
luckylinux <luckylinux>
2025-06-08 15:25:27 +0200

    Add initial Migration Script.

---
+++ migrate_system_to_another_disk.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Determine toolpath if not set already
+relativepath="./" # Define relative path to go from this script to the root level of the tool
+if [[ ! -v toolpath ]]; then scriptpath=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd ); toolpath=$(realpath --canonicalize-missing $scriptpath/$relativepath); fi
+
+# Load configuration
+source "${toolpath}/load.sh"
+
+# Umount everything that was previosly mounted
+source ${toolpath}/umount_everything.sh
+
+# Ensure that the mointpoint exists and is empty
+source ${toolpath}/modules/setup_mountpoint.sh
+
+# Setup Requirements for the Installation (Packages will be installed on the currently running HOST)
+installroot="" # Needed to ensure that we install on the Host
+source ${toolpath}/modules/setup_requirements.sh
+
+# Umount everything that was previosly mounted
+source ${toolpath}/umount_everything.sh
+
+# Init partitioning
+source ${toolpath}/modules/init_partitioning.sh
+
+# Setup disks
+source ${toolpath}/modules/setup_partitions.sh
+
+# Migrate system
+source ${toolpath}/modules/migrate_system.sh
+
+# Mount system
+source ${toolpath}/modules/mount_system.sh
+
+# Mount bind
+source ${toolpath}/modules/mount_bind.sh
+
+# Setup chroot
+source ${toolpath}/modules/setup_chroot.sh
---
+++ modules/migrate_system.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Determine toolpath if not set already
+relativepath="../" # Define relative path to go from this script to the root level of the tool
+if [[ ! -v toolpath ]]; then scriptpath=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd ); toolpath=$(realpath --canonicalize-missing $scriptpath/$relativepath); fi
+
+# Load configuration
+source "${toolpath}/load.sh"
+
+# Check to See if restore_from_mountpoint is set and exists
+if [ -z "${restore_from_mountpoint}" ] || [ ! -d "${restore_from_mountpoint}" ]
+then
+    # Ask User Input
+    read -p "Enter the Mountpoint where the System to Migrate is currently Mounted: " restore_from_mountpoint
+fi
+
+# Check that it's mounted
+if mountpoint -q "${restore_from_mountpoint}"
+then
+    # Check if /boot
+    if ! mountpoint -q "${restore_from_mountpoint}/boot"
+    then
+        read -p "WARNING: ${restore_from_mountpoint}/boot does NOT contain a mounted Filesystem. Continue ? [yes/no]: "  keep_going
+
+        if [[ "${keep_going}" != "yes" ]]
+        then
+           exit 8
+        fi
+    fi
+
+    # Check if /boot/efi are mounted
+    if ! mountpoint -q "${restore_from_mountpoint}/boot/efi"
+    then
+        read -p "WARNING: ${restore_from_mountpoint}/boot/efi does NOT contain a mounted Filesystem. Continue ? [yes/no]: "  keep_going
+
+        if [[ "${keep_going}" != "yes" ]]
+        then
+           exit 8
+        fi
+    fi
+
+    # Copy Data using rsync
+    rsync -aPAXUHEtv --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found"} ${restore_from_mountpoint}/ ${destination}
+else
+    # Abort
+    echo "ERROR: Path ${restore_from_mountpoint} does NOT contain a mounted Filesystem. Aborting !"
+    exit 9
+fi
