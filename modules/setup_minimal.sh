becc420888414b29132c5b9dbd22131cd0faa43c
luckylinux <luckylinux>
2025-06-14 09:12:49 +0200

    Fix Debian-Family specific File Copy.

--- modules/setup_minimal.sh
+++ modules/setup_minimal.sh
@@ -115,19 +115,30 @@ done
 #        fi
 #fi
 
-# Install minimal system
-debootstrap --exclude="${excludepackages}" --include="${includepackages}" "${release}" "${destination}" "${source}"
+if [[ "${DISTRIBUTION_FAMILY}" == "debian" ]]
+then
+    # Install minimal system
+    debootstrap --exclude="${excludepackages}" --include="${includepackages}" "${release}" "${destination}" "${source}"
+elif [[ "${DISTRIBUTION_FAMILY}" == "fedora" ]]
+then
+    # Not implemented yet
+    # Most likely will need to download and extract a RootFS base Image
+    x=1
+fi
 
 # Bind required filesystems
 source ${toolpath}/modules/mount_bind.sh
 
-# Copy APT sources
-cp "${toolpath}/repositories/${distribution}/${release}/sources.list" "${destination}/etc/apt/sources.list"
+if [[ "${DISTRIBUTION_FAMILY}" == "debian" ]]
+then
+    # Copy APT sources
+    cp "${toolpath}/repositories/${distribution}/${release}/sources.list" "${destination}/etc/apt/sources.list"
 
-# Copy GRUB configuration files
-mkdir -p "${destination}/etc/default/grub.d"
-cp "${toolpath}/files/etc/default/grub" "${destination}/etc/default/grub"
-cp -ar ${toolpath}/files/etc/default/grub.d/* "${destination}/etc/default/grub.d/"
+    # Copy GRUB configuration files
+    mkdir -p "${destination}/etc/default/grub.d"
+    cp "${toolpath}/files/etc/default/grub" "${destination}/etc/default/grub"
+    cp -ar ${toolpath}/files/etc/default/grub.d/* "${destination}/etc/default/grub.d/"
+fi
 
 if [[ -f "${destination}/etc/default/grub.d/zfs.cfg" ]]
 then
@@ -156,30 +167,34 @@ ifconfig
 echo "Possible network interface names are: " && ls /sys/class/net/
 read -p "Enter network interface name: " interfacename
 
-mkdir -p "${destination}/etc/network/interfaces.d"
-
 # Create Persistent Interface Name using UDEV
 macaddress=$(find /sys/class/net -mindepth 1 -maxdepth 1 ! -name lo -printf "%P: " -execdir cat {}/address \; | grep ${interfacename} | sed -E "s|${interfacename}:\s*?([0-9a-fA-F:]+)$|\1|")
 
 echo "SUBSYSTEM==\"net\", ACTION==\"add\", DRIVERS==\"?*\", ATTR{address}==\"${macaddress}\", ATTR{type}==\"1\", KERNEL==\"*\", NAME=\"${interfacename}\"" >> "${destination}/etc/udev/rules.d/30-net_persistent_names.rules"
 
-if [[ "${ipconfiguration}" == "static" ]]
+
+if [[ "${DISTRIBUTION_FAMILY}" == "debian" ]]
 then
-   echo "auto ${interfacename}" > "${destination}/etc/network/interfaces.d/${interfacename}"
-   #echo "allow-hotplug ${interfacename}" >> "${destination}/etc/network/interfaces.d/${interfacename}" # Should prevent hanging during boot process
-   echo "iface ${interfacename} inet static" >> "${destination}/etc/network/interfaces.d/${interfacename}"
-   echo "	address ${ipaddress}" >> "${destination}/etc/network/interfaces.d/${interfacename}"
-   echo "	netmask ${subnetmask}" >> "${destination}/etc/network/interfaces.d/${interfacename}"
-   echo "	gateway ${defgateway}" >> "${destination}/etc/network/interfaces.d/${interfacename}"
-elif [[ "${ipconfiguration}" == "dhcp" ]]
-then
-   echo "auto ${interfacename}" > "${destination}/etc/network/interfaces.d/${interfacename}"
-   #echo "allow-hotplug ${interfacename}" >> "${destination}/etc/network/interfaces.d/${interfacename}" # Should prevent hanging during boot process
-   echo "iface ${interfacename} inet dhcp" >> "${destination}/etc/network/interfaces.d/${interfacename}"
-else
-   echo "ERROR: Network Configuration Type <${ipconfiguration}> is NOT supported. Must be one of: <static> or <dhcp>"
-   echo "ABORTING ..."
-   exit 9
+    mkdir -p "${destination}/etc/network/interfaces.d"
+
+    if [[ "${ipconfiguration}" == "static" ]]
+    then
+        echo "auto ${interfacename}" > "${destination}/etc/network/interfaces.d/${interfacename}"
+        #echo "allow-hotplug ${interfacename}" >> "${destination}/etc/network/interfaces.d/${interfacename}" # Should prevent hanging during boot process
+        echo "iface ${interfacename} inet static" >> "${destination}/etc/network/interfaces.d/${interfacename}"
+        echo "	address ${ipaddress}" >> "${destination}/etc/network/interfaces.d/${interfacename}"
+        echo "	netmask ${subnetmask}" >> "${destination}/etc/network/interfaces.d/${interfacename}"
+        echo "	gateway ${defgateway}" >> "${destination}/etc/network/interfaces.d/${interfacename}"
+    elif [[ "${ipconfiguration}" == "dhcp" ]]
+    then
+        echo "auto ${interfacename}" > "${destination}/etc/network/interfaces.d/${interfacename}"
+        #echo "allow-hotplug ${interfacename}" >> "${destination}/etc/network/interfaces.d/${interfacename}" # Should prevent hanging during boot process
+        echo "iface ${interfacename} inet dhcp" >> "${destination}/etc/network/interfaces.d/${interfacename}"
+    else
+        echo "ERROR: Network Configuration Type <${ipconfiguration}> is NOT supported. Must be one of: <static> or <dhcp>"
+        echo "ABORTING ..."
+        exit 9
+    fi
 fi
 
 echo "Network configured for <${interfacename}> in <${ipconfiguration}> mode"
@@ -188,7 +203,7 @@ echo "Network configured for <${interfacename}> in <${ipconfiguration}> mode"
 if [[ "$setupnfstools" == "yes" ]]
 then
    echo "# Tools over NFS" >> "${destination}/etc/fstab"
-   echo "192.168.1.223:/export/tools          /tools_nfs           nfs             rw,user=tools,auto,nfsvers=3          0       0"  >> "${destination}/etc/fstab"
+   echo "192.168.1.223:/export/tools            /tools_nfs      nfs     rw,user=tools,auto,nofail,x-systemd.automount,mountvers=4.0,proto=tcp        0   0"  >> "${destination}/etc/fstab"
    mkdir -p  "${destination}/tools_nfs"
    chattr +i "${destination}/tools_nfs"
 fi
--- modules/setup_zfs_backports.sh
+++ modules/setup_zfs_backports.sh
@@ -4,37 +4,40 @@
 relativepath="../" # Define relative path to go from this script to the root level of the tool
 if [[ ! -v toolpath ]]; then scriptpath=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd ); toolpath=$(realpath --canonicalize-missing $scriptpath/$relativepath); fi
 
-# Get Debian Version
-echo "Configuring System <${distribution} ${release}> to use Backports Packages for ZFS"
-
-# Create folder if not exist
-mkdir -p ${installroot}/etc
-mkdir -p ${installroot}/etc/apt
-mkdir -p ${installroot}/etc/apt/sources.list.d
-mkdir -p ${installroot}/etc/apt/preferences.d
-
-# Copy Backports Definition
-cp ${toolpath}/repositories/${distribution}/${release}/sources.list.d/${distribution}-backports.list ${installroot}/etc/apt/sources.list.d/${distribution}-backports.list
-
-# By default do NOT use backports
-cp ${toolpath}/repositories/${distribution}/${release}/preferences.d/${distribution}-backports ${installroot}/etc/apt/preferences.d/${distribution}-backports
-
-
-# Also use Kernel Backports ?
-if [[ "${usekernelbackports}" == "yes" ]]
+if [[ "${DISTRIBUTION_FAMILY}" == "debian" ]]
 then
-   # Copy Kernel Backports Configuration
-   cp ${toolpath}/repositories/${distribution}/${release}/preferences.d/kernel-backports ${installroot}/etc/apt/preferences.d/kernel-backports
+   # Get Debian Version
+   echo "Configuring System <${distribution} ${release}> to use Backports Packages for ZFS"
+
+   # Create folder if not exist
+   mkdir -p ${installroot}/etc
+   mkdir -p ${installroot}/etc/apt
+   mkdir -p ${installroot}/etc/apt/sources.list.d
+   mkdir -p ${installroot}/etc/apt/preferences.d
+
+   # Copy Backports Definition
+   cp ${toolpath}/repositories/${distribution}/${release}/sources.list.d/${distribution}-backports.list ${installroot}/etc/apt/sources.list.d/${distribution}-backports.list
+
+   # By default do NOT use backports
+   cp ${toolpath}/repositories/${distribution}/${release}/preferences.d/${distribution}-backports ${installroot}/etc/apt/preferences.d/${distribution}-backports
+
+
+   # Also use Kernel Backports ?
+   if [[ "${usekernelbackports}" == "yes" ]]
+   then
+      # Copy Kernel Backports Configuration
+      cp ${toolpath}/repositories/${distribution}/${release}/preferences.d/kernel-backports ${installroot}/etc/apt/preferences.d/kernel-backports
+   fi
+
+   # Copy ZFS Backports Configuration
+   cp ${toolpath}/repositories/${distribution}/${release}/preferences.d/zfs-backports ${installroot}/etc/apt/preferences.d/zfs-backports
+
+   # Update Sources
+   update_lists
+
+   # Perform Installation
+   install_packages zfsutils-linux zfs-dkms
+
+   # Upgrade Kernel Packages if applicable
+   upgrade_packages
 fi
-
-# Copy ZFS Backports Configuration
-cp ${toolpath}/repositories/${distribution}/${release}/preferences.d/zfs-backports ${installroot}/etc/apt/preferences.d/zfs-backports
-
-# Update Sources
-update_lists
-
-# Perform Installation
-install_packages zfsutils-linux zfs-dkms
-
-# Upgrade Kernel Packages if applicable
-upgrade_packages
