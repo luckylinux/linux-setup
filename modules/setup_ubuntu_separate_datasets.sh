a5101edc1801f771b2853f5631f320b06b8af924
luckylinux <luckylinux>
2025-10-26 08:03:28 +0100

    Mount ZFS Datasets for Ubuntu Setup.

--- modules/setup_ubuntu_separate_datasets.sh
+++ modules/setup_ubuntu_separate_datasets.sh
@@ -12,63 +12,75 @@ mkdir -p ${destination}/srv
 chattr +i ${destination}/srv
 zfs create -o com.ubuntu.zsys:bootfs=no \
     ${rootpool}/ROOT/$distribution/srv
+zfs mount ${rootpool}/ROOT/$distribution/srv
 
 
 mkdir -p ${destination}/usr
 chattr +i ${destination}/usr
 zfs create -o com.ubuntu.zsys:bootfs=no -o canmount=on \
     ${rootpool}/ROOT/$distribution/usr
+zfs mount ${rootpool}/ROOT/$distribution/usr
 
 
 mkdir -p ${destination}/usr
 chattr +i ${destination}/usr
 zfs create ${rootpool}/ROOT/$distribution/usr/local
+zfs mount ${rootpool}/ROOT/$distribution/usr/local
 
 
 mkdir -p ${destination}/var
 chattr +i ${destination}/var
 zfs create -o com.ubuntu.zsys:bootfs=no -o canmount=on \
     ${rootpool}/ROOT/$distribution/var
+zfs mount ${rootpool}/ROOT/$distribution/var
 
 
 mkdir -p ${destination}/srv
 chattr +i ${destination}/srv
 zfs create ${rootpool}/ROOT/$distribution/var/games
+zfs mount ${rootpool}/ROOT/$distribution/var/games
 
 
 mkdir -p ${destination}/var/lib
 chattr +i ${destination}/var/lib
 zfs create ${rootpool}/ROOT/$distribution/var/lib
+zfs mount ${rootpool}/ROOT/$distribution/var/lib
 
 
 mkdir -p ${destination}/var/lib/AccountsService
 chattr +i ${destination}/var/lib/AccountsService
 zfs create ${rootpool}/ROOT/$distribution/var/lib/AccountsService
+zfs mount ${rootpool}/ROOT/$distribution/var/lib/AccountsService
 
 
 mkdir -p ${destination}/var/lib/apt
 chattr +i ${destination}/var/lib/apt
 zfs create ${rootpool}/ROOT/$distribution/var/lib/apt
+zfs mount ${rootpool}/ROOT/$distribution/var/lib/apt
 
 
 mkdir -p ${destination}/var/lib/dpkg
 chattr +i ${destination}/var/lib/dpkg
 zfs create ${rootpool}/ROOT/$distribution/var/lib/dpkg
+zfs mount ${rootpool}/ROOT/$distribution/var/lib/dpkg
 
 
 mkdir -p ${destination}/var/lib/NetworkManager
 chattr +i ${destination}/var/lib/NetworkManager
 zfs create ${rootpool}/ROOT/$distribution/var/lib/NetworkManager
+zfs mount ${rootpool}/ROOT/$distribution/var/lib/NetworkManager
 
 
 mkdir -p ${destination}/var/log
 chattr +i ${destination}/var/log
 zfs create ${rootpool}/ROOT/$distribution/var/log
+zfs mount ${rootpool}/ROOT/$distribution/var/log
 
 
 mkdir -p ${destination}/var/mail
 chattr +i ${destination}/var/mail
 zfs create ${rootpool}/ROOT/$distribution/var/mail
+zfs mount ${rootpool}/ROOT/$distribution/var/mail
 
 
 mkdir -p ${destination}/var/snap
@@ -79,11 +91,13 @@ chattr +i ${destination}/var/snap
 mkdir -p ${destination}/var/spool
 chattr +i ${destination}/var/spool
 zfs create ${rootpool}/ROOT/$distribution/var/spool
+zfs mount ${rootpool}/ROOT/$distribution/var/spool
 
 
 mkdir -p ${destination}/var/www
 chattr +i ${destination}/var/www
 zfs create ${rootpool}/ROOT/$distribution/var/www
+zfs mount ${rootpool}/ROOT/$distribution/var/www
 
 
 
@@ -95,6 +109,7 @@ zfs create -o canmount=off -o mountpoint=/ \
 mkdir -p ${destination}/var/www
 chattr +i ${destination}/var/www
 zfs create ${rootpool}/ROOT/$distribution/var/lib/libvirt
+zfs mount ${rootpool}/ROOT/$distribution/var/lib/libvirt
 
 
 
@@ -103,6 +118,7 @@ chattr +i ${destination}/root
 zfs create -o com.ubuntu.zsys:bootfs-datasets=rpool/ROOT/$distribution \
     -o canmount=on -o mountpoint=/root \
     ${rootpool}/USERDATA/root
+zfs mount ${rootpool}/USERDATA/root
 
 
 
@@ -110,7 +126,7 @@ mkdir -p ${destination}/tmp
 chattr +i ${destination}/tmp
 zfs create -o com.ubuntu.zsys:bootfs=no \
     ${rootpool}/ROOT/$distribution/tmp
-
+zfs mount ${rootpool}/ROOT/$distribution/tmp
 
 
 
