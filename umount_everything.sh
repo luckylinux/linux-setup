236ac743216f61b26c689fd74fe5422dd6975585
luckylinux <luckylinux>
2026-01-03 13:41:19 +0100

    Repeat unmount Operation 2 Times. Repeat unmount Operation 2 Times inside umount_everything.sh to ensure that everything is properly unmounted.

--- umount_everything.sh
+++ umount_everything.sh
@@ -7,24 +7,31 @@ if [[ ! -v toolpath ]]; then scriptpath=$(cd "$( dirname "${BASH_SOURCE[0]}" )" 
 # Load configuration
 source "${toolpath}/load.sh"
 
-# Umount previosuly mounted bind mounts
-source ${toolpath}/modules/umount_bind.sh
+# Number of Times to repeat to ensure that everything is properly unmounted
+# 2 is usually a good Value
+NUM_UMOUNT_REPEAT=2
 
-# Force Chroot Processes to stop
-source ${toolpath}/modules/stop_chroot_processes.sh
+for i in $(seq 1 1 ${NUM_UMOUNT_REPEAT})
+do
+    # Umount previosuly mounted bind mounts
+    source ${toolpath}/modules/umount_bind.sh
 
-# Umount previosuly mounted Chroot
-source ${toolpath}/modules/umount_chroot.sh
+    # Force Chroot Processes to stop
+    source ${toolpath}/modules/stop_chroot_processes.sh
 
-# Umount previosuly mounted ZFS Pools
-source ${toolpath}/modules/umount_zfs.sh
+    # Umount previosuly mounted Chroot
+    source ${toolpath}/modules/umount_chroot.sh
 
-# Umount previosuly assembled MDADM Arrays
-source ${toolpath}/modules/umount_mdadm.sh
+    # Umount previosuly mounted ZFS Pools
+    source ${toolpath}/modules/umount_zfs.sh
 
-# Umount everything that remains by force
-source ${toolpath}/modules/umount_force_all.sh
+    # Umount previosuly assembled MDADM Arrays
+    source ${toolpath}/modules/umount_mdadm.sh
 
-# Echo for difficult cases
-echo -e "\nWARNING: In case of errors it might be easier to just REBOOT the system\n"
-sleep 5
+    # Umount everything that remains by force
+    source ${toolpath}/modules/umount_force_all.sh
+
+    # Echo for difficult cases
+    echo -e "\nWARNING: In case of errors it might be easier to just REBOOT the system\n"
+    sleep 5
+done
